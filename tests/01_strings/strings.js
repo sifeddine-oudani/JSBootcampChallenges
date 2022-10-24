babfde4baa68dfed43b388789ddef78b73242897
thesife <37145349+thesife@users.noreply.github.com>
2022-10-24 23:35:27 +0100

    Update strings.js

--- tests/01_strings/strings.js
+++ tests/01_strings/strings.js
@@ -1,18 +1,25 @@
 /* eslint-disable no-unused-vars */
 
 function doesInclude(str, letter) {
-
+  return str.includes( letter );
 }
 
 function isPalindrome(str) {
-
+  return str.split("").reverse().join("") === str;
 }
 
 function cap(str, letter) {
-
+  if (str.indexOf(letter) !== -1) {
+    let char = str.indexOf(letter);
+    return str[char + 1].toUpperCase();
+  } else {
+    return "sorry not found";
+  }
 }
 
 function firstCharacter(str1, str2) {
+  
+  return str1[0].toLowerCase() === str2[0].toLowerCase() ? true : false;
 
 }
 
